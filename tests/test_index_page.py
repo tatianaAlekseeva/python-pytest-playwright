54702ab2888048ee8dc2fefa94a63dee8b8a4804
TAlekseeva <TAlekseeva@luxoft.com>
2024-05-28 10:18:12 +0200

    first test name fix

--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -5,6 +5,6 @@ import time
 
 class TestFooter:
 
-    def test_user_should_be_able_to_open_popup_select_subscription_plan(self, page):
+    def test_the_main_search_button_must_have_the_text_google_search(self, page):
         pages.index_page.open_index_page(page)
         time.sleep(10)
